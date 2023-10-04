f45bfcac462de437bfea5bb2fd36f747d7dee60f
yanisquinto <yanis.quinto@mail.ch>
2023-10-04 09:38:32 +0200

    debug de la modale de la gallerie d'images

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -120,7 +120,7 @@
       $(`#${lightboxId}`).modal("toggle");
     },
     prevImage() {
-      let activeImage = null;
+      let activeImage = $(".lightboxImage");
       $("img.gallery-item").each(function() {
         if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
           activeImage = $(this);
@@ -154,12 +154,12 @@
         }
       });
       next =
-        imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
+        imagesCollection[index - 1] ||
+        imagesCollection[imagesCollection[imagesCollection.length - 1]];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     nextImage() {
-      let activeImage = null;
+      let activeImage = $(".lightboxImage");
       $("img.gallery-item").each(function() {
         if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
           activeImage = $(this);
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
